420c463949fc20d4fc55548b407feefbcf028d6a
ali <rima.aneta@gmail.com>
2020-11-14 19:29:11 +0100

    new classes created

--- Library/Program.cs
+++ Library/Program.cs
@@ -13,4 +13,72 @@ namespace Library
 
         }
     }
+    class Library
+    {
+        Employee[] employee = new Employee[20];     // defult number of employees: 20 persons
+        private void AddEmployee()
+        {
+            string name;
+            int age;
+            int maxAgeLimit = 65;
+            int minAgeLimit = 18;
+
+            Console.Clear();
+            Console.WriteLine("####### ADD EMPLOYEE #######");
+            //Write name
+            while (true)
+            {
+                Console.WriteLine("Employee name:");
+                try
+                {
+                    name = Console.ReadLine();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            //Enter gender
+            while (true)
+            {
+                Console.WriteLine("Employee age:");
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+                    if(age > minAgeLimit && age < maxAgeLimit)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Employees age is invalid");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+    }
+    class Employee
+    {
+        public Employee(string name, int age, string gender, string email, int pass)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Email = email;
+            Pass = pass;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public int Pass { get; set; }
+
+    }
 }
