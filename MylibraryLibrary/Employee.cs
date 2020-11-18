bbf12a7eb4cf9fa0bc3c6f4baca58196d1d7e7c3
ali <rima.aneta@gmail.com>
2020-11-18 17:43:55 +0100

    some classes removed, only Program class left because i created a new own Library with two classes as Employee and Admin

---
+++ MylibraryLibrary/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MylibraryLibrary
+{
+    public class Employee
+    {
+        public Employee(int id, string name, string gender, string email, int pass)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Email = email;
+            Pass = pass;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public int Pass { get; set; }
+
+        public string GenderPronoum(string gender)
+        {
+            switch (gender)
+            {
+                case "X":
+                    gender = "Female";
+                    break;
+                case "Y":
+                    gender = "Male";
+                    break;
+            }
+            return gender;
+        }
+    }
+}
